e1c06a536dacea923c49a77288c5071843e7e04f
KevinLZC <kevin_lazarillo@hotmail.com>
2024-02-15 22:42:04 -0600

    CSS changes

--- js/app.js
+++ js/app.js
@@ -58,8 +58,8 @@ function addItemTable(item) {
       <td>${item.getBrand}</td>
       <td>${item.getPrice}</td>
       <td>
-        <button onclick="setInputItem('${item.getId}')">Editar</button>
-        <button onclick="deleteItem('${item.getId}')">Eliminar</button>
+        <a onclick="setInputItem('${item.getId}')"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="#ea580c" d="M5 19h1.425L16.2 9.225L14.775 7.8L5 17.575zm-2 2v-4.25L16.2 3.575q.3-.275.663-.425t.762-.15q.4 0 .775.15t.65.45L20.425 5q.3.275.438.65T21 6.4q0 .4-.137.763t-.438.662L7.25 21zM19 6.4L17.6 5zm-3.525 2.125l-.7-.725L16.2 9.225z"/></svg></a>
+        <a onclick="deleteItem('${item.getId}')"><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24"><path fill="#dc2626" d="m9.4 16.5l2.6-2.6l2.6 2.6l1.4-1.4l-2.6-2.6L16 9.9l-1.4-1.4l-2.6 2.6l-2.6-2.6L8 9.9l2.6 2.6L8 15.1zM7 21q-.825 0-1.412-.587T5 19V6H4V4h5V3h6v1h5v2h-1v13q0 .825-.587 1.413T17 21z"/></svg></a>
       </td>
     </tr>`
 }
