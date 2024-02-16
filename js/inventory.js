43fcc448a7add8e072e44b05a40f50568efa4839
KevinLZC <kevin_lazarillo@hotmail.com>
2024-02-16 12:17:21 -0600

    Corrections added

--- js/inventory.js
+++ js/inventory.js
@@ -1,12 +1,6 @@
 class Inventory {
   constructor() {
-    this.inventory = [
-      new Item('AmG66f7', 'Laptop', 'Inspiron', 'Dell', 1000),
-      new Item('EBcIfIj', 'Smartphone', 'Galaxy', 'Samsung', 800),
-      new Item('zttAbsi', 'Tablet', 'iPad', 'Apple', 600),
-      new Item('MXdgit8', 'Smartwatch', 'Watch', 'Apple', 400),
-      new Item('nzblsBl', 'Headphones', 'Beats', 'Apple', 300)
-    ]
+    this.inventory = []
   }
 
   get getInventory() {
@@ -19,7 +13,11 @@ class Inventory {
   }
 
   deleteItem(id) {
-    this.inventory = this.inventory.filter(item => item.getId !== id)
-    window.alert(`Se eliminó el producto con ID ${id} de forma correcta`);
+    for (let i = 0; i < this.inventory.length; i++) {
+      if (this.inventory[i].id === id) {
+        this.inventory.splice(i, 1)
+        window.alert(`Se eliminó el producto con ID ${id} de forma correcta`);
+      }
+    }
   }
 }
